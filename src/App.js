02e20418890fb4ac692d82b18c91b6bd430809d0
Enricco Diniz <enriccodiniz@pop-os.localdomain>
2020-05-19 19:39:36 -0300

    Commit apos conclusao do desafio | 19/05 19:40

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React from "react";
-
+import React ,{ useEffect, useState }from "react";
+import api from  "./services/api"
 import "./styles.css";
 
 function App() {
+  const [reposList, setRepos] = useState([]);
+  const [inputTxt, updateInput] = useState("");
+
+  useEffect(()=>{
+    api.get("repositories").then(response=>{
+      setRepos(response.data)
+    })
+  },[])
+  
+  function handleTyping(event){
+    updateInput(event.target.value)
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post(`repositories`,{
+      "title": inputTxt, 
+      "url": "http://github.com/...", 
+      "techs": ["Node.js", "..."]
+    })
+    setRepos(response.data)
+    updateInput("");
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`)
+    setRepos(response.data)
   }
 
   return (
-    <div>
+    <div className="container">
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {
+        reposList.map(repo=>{
+          return  (      
+          <li key={repo.id}>
+            <div>
+            {repo.title}
+            </div>
+          
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        )})
+      }
       </ul>
-
+      <input type="text" value={inputTxt} onChange={handleTyping}></input>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
